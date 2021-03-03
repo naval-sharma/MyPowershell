86178db4b62995e52d0328186a6dfaabad5121e2
Naval Sharma <sharma.naval@outlook.com>
2021-03-04 01:51:14 +0530

    v1

---
+++ Get-ADDitLocationSize.ps1
@@ -0,0 +1,15 @@
+##This code collects the DCs in the domain and the size of the NTDS.dit on each DC##
+$dcs = (Get-ADDomainController).Name
+$dbs = Invoke-Command -ComputerName $dcs -ScriptBlock {
+    Get-ItemProperty -Path HKLM:\System\CurrentControlSet\Services\NTDS\Parameters
+} | Select-Object PSComputerName,'DSA Database File'
+$dbs | ForEach-Object {
+    $output = @{}
+    $path = $_.'DSA Database File'
+    $output.Add('DomainController', $_.PSComputerName)
+    $size = Invoke-Command -ComputerName $_.PSComputerName -ScriptBlock {
+        (Get-ItemProperty -Path $using:path).Length /1GB
+    }
+    $output.Add('DatabaseSizeDB', $size)
+    [pscustomobject]$output
+}
